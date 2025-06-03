2497fff071f22335d1e2e6e2be27b1d03a2d01bc
Saifullah Haaris <101180099+saifullahhaaris@users.noreply.github.com>
2025-06-03 23:46:44 +0530

    Update MovieDatabase.kt

--- app/src/main/java/com/example/mymovieapplication/data/api/OmdbApiService.kt
+++ app/src/main/java/com/example/mymovieapplication/data/api/OmdbApiService.kt
@@ -1,3 +1,6 @@
+// https://www.omdbapi.com/?t=Matrix&apikey=YOUR_OWN_API_KEY
+
+
 package com.example.mymovieapplication.data.api
 
 import com.example.mymovieapplication.data.MovieEntity
@@ -8,12 +11,12 @@ interface OmdbApiService {
     @GET("/")
     suspend fun searchMovie(
         @Query("t") title: String,
-        @Query("apikey") apiKey: String = "b1bd51b2"
+        @Query("apikey") apiKey: String = "YOUR_OWN_API_KEY"
     ): MovieEntity
 
     @GET("/")
     suspend fun searchMoviesByTitle(
         @Query("s") query: String,
-        @Query("apikey") apiKey: String = "b1bd51b2"
+        @Query("apikey") apiKey: String = "YOUR_OWN_API_KEY"
     ): SearchResponse
 }
